b0228416ccbf5495a0c61f29f93f79c03253a1b6
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2022-10-08 16:06:49 +0545

    added ENV

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import "firebase/auth";
 
 export const auth = firebase
   .initializeApp({
-    apiKey: "AIzaSyCXDp2J7AOKEM1JB44zIQnuGoF_RtlLvO0",
-    authDomain: "tchat-18b1d.firebaseapp.com",
-    projectId: "tchat-18b1d",
+    apiKey: process.env.REACT_APP_AUTH_API,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_AUTH_ID,
     storageBucket: "tchat-18b1d.appspot.com",
     messagingSenderId: "1033104274531",
-    appId: "1:1033104274531:web:bbc92d3d900f6da5d97456",
+    appId: process.env.REACT_APP_AUTH_APP_ID,
   })
   .auth();
